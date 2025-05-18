3d804d2d9f13e2d0c9b376aaaad5a29d5b15e1d7
khadijaDev <khadijaoukhlift@gmail.com>
2025-05-18 21:44:00 +0200

    Budget & category service

--- backend/src/main/java/org/budgetmanager/backend/service/BudgetService.java
+++ backend/src/main/java/org/budgetmanager/backend/service/BudgetService.java
@@ -1,4 +1,12 @@
 package org.budgetmanager.backend.service;
 
-public class BudgetService {
+import org.budgetmanager.backend.dto.BudgetDTO;
+
+import java.util.List;
+
+public interface BudgetService {
+    BudgetDTO createBudget(BudgetDTO dto);
+    List<BudgetDTO> getAllBudgets();
+    BudgetDTO updateBudget(Long id, BudgetDTO dto);
+    void deleteBudget(Long id);
 }
